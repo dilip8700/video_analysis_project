79daee217a54a4d3c0d98087d56763c89b0c9446
Dilip Jarwal <99677060+dilip8700@users.noreply.github.com>
2025-01-16 19:58:31 +0530

    Create video.py

---
+++ video.py
@@ -0,0 +1,62 @@
+import os
+import time
+import google.generativeai as genai
+
+video_path="enter you video path"
+genai.configure(api_key="enter you api key")
+
+def upload_to_gemini(path, mime_type=None):
+  """Uploads the given file to Gemini.
+
+  See https://ai.google.dev/gemini-api/docs/prompting_with_media
+  """
+  file = genai.upload_file(path, mime_type=mime_type)
+  print(f"Uploaded file '{file.display_name}' as: {file.uri}")
+  return file
+
+def wait_for_files_active(files):
+  print("Waiting for file processing...")
+  for name in (file.name for file in files):
+    file = genai.get_file(name)
+    while file.state.name == "PROCESSING":
+      print(".", end="", flush=True)
+      time.sleep(10)
+      file = genai.get_file(name)
+    if file.state.name != "ACTIVE":
+      raise Exception(f"File {file.name} failed to process")
+  print("...all files ready")
+  print()
+
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 40,
+  "max_output_tokens": 8192,
+  "response_mime_type": "application/json",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-pro",
+  generation_config=generation_config,
+)
+
+files = [
+  upload_to_gemini(video_path, mime_type="video/mp4"),
+]
+
+wait_for_files_active(files)
+
+chat_session = model.start_chat(
+  history=[
+    {
+      "role": "user",
+      "parts": [
+        files[0],
+      ],
+    },
+  ]
+)
+
+response = chat_session.send_message("tell me how many color they used in this video")
+
+print(response.text)
